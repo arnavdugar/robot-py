ef2e20381ed74ca778d88a14c4f31417dc44018e
Arnav Dugar <arnavdugar@gmail.com>
2014-12-11 18:33:04 -0800

    Snapshot.

--- insect.py
+++ insect.py
@@ -10,9 +10,17 @@ class Leg(robot.Leg):
     def init(self):
         self.name = self.name_base + ' Leg'
         self.segments = serialize.load(self.model)
+        initial_direction = self.segments[0].direction_center
+        angle = initial_direction.angle(self.leg_direction)
+        axis = initial_direction.cross(self.leg_direction)
         for i in range(len(self.segments)):
             segment = self.segments[i]
+            segment.index = i
+            segment.parent = self
             segment.name = Leg.segment_types[i] + ' ' + self.name_base + ' Segment'
             segment.length = self.lengths[i]
             segment.write_index = self.write_indexes[i]
+            segment.axis = segment.axis.rotated(axis, angle)
+            segment.direction_center = segment.direction_center.rotated(axis, angle)
+            segment.init()
         self.segments[0].parent = self
